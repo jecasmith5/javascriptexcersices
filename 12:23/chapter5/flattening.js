bc5108c5447ba3e817caf95a420bd13137e4040d
Jessica Smith <jecasmith5@ymail.com>
2017-01-05 11:25:54 -0600

    first try

--- 12:23/chapter5/flattening.js
+++ 12:23/chapter5/flattening.js
@@ -1,7 +1,18 @@
-var array = [[1, 2, 3], [4, 5], [6]];
-// Your code here.
-// → [1, 2, 3, 4, 5, 6]
+// var array = [[1, 2, 3], [4, 5], [6]];
+// // Your code here.
+// // → [1, 2, 3, 4, 5, 6]
+//
+// console.log(array.reduce(function(flat, current) {
+//   return flat.concat(current);
+// }, []));
 
-console.log(array.reduce(function(flat, current) {
-  return flat.concat(current);
-}, []));
+
+var notFlatArray = [[1, 2, 3], [4, 5], [6]];
+
+function flatArray(flatten){
+  console.log(flatten.reduce(function(flatten,neww){
+    return flatten.concat(neww);
+  },[]));
+
+}
+flatArray(notFlatArray);
