f8b34c9204f939473a092ae2f825e2747ed846bb
Jessica Smith <jecasmith5@ymail.com>
2017-01-10 16:55:38 -0600

    chapter5 ex1

---
+++ additionalexercises/chapter5/ex1.js
@@ -0,0 +1,38 @@
+var accounts=[
+  {accountNumber:"A042718",accountBalance:500.00},
+  {accountNumber:"A042719",accountBalance: 1000.00}
+];
+var balance=account.accountBalance;
+var number=account.accountNumber;
+console.log(balance);
+console.log(number);
+
+
+// function account(){
+//   this.number="";
+//   this.balance="";
+// }
+function deposit(x,number){
+  if(x > balance){
+    console.log("deposit can not be made");
+  }else{
+    balance += x;
+    console.log('your balance is' + " " + balance);
+  }
+
+
+deposit(400.00,number);
+
+
+// A bank that holds client’s information:
+// account number;
+// balance
+
+// A set of clients where each can:
+// hold money of their own;
+// deposit money into the bank (to any account);
+// retrieve money from the bank (from personal account only);
+// view current balance in bank (from personal account only)
+// A client cannot deposit more money than what it has;
+// A client cannot retrieve more money that what is in its account;
+// All financial information must be private
