80e8b33d101ea7dd3982ca0ae2328f3d8abf46dd
Jessica Smith <jecasmith5@ymail.com>
2017-01-10 13:13:39 -0600

    com

--- 12:23/chapter4/sum.js
+++ 12:23/chapter4/sum.js
@@ -1,20 +1,49 @@
-function range(start,end,next){
-  if  {
-      for (var i = start; i >= end; i += next)
-        array.push(i);
-      }
-      return array;
-}
-function sum(array) {
-  var total = 0;
-  for (var i = 0; i < array.length; i++)
-    total += array[i];
-  return total;
+function range(){
+  if {
+    for (var i = 0; i < start; i++) {
+      start.push[i]
+    }
+  }
 }
 
-console.log(range(1, 10));
-// → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-console.log(range(5, 2, -1));
-// → [5, 4, 3, 2]
-console.log(sum(range(1, 10)));
-// → 55
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function range(start,end,next){
+//   if  {
+//       for (var i = start; i >= end; i += next)
+//         array.push(i);
+//       }
+//       return array;
+// }
+// function sum(array) {
+//   var total = 0;
+//   for (var i = 0; i < array.length; i++)
+//     total += array[i];
+//   return total;
+// }
+//
+// console.log(range(1, 10));
+// // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// console.log(range(5, 2, -1));
+// // → [5, 4, 3, 2]
+// console.log(sum(range(1, 10)));
+// // → 55
